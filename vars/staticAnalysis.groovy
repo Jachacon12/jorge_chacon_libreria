c609ca01492c8f6e07457aa2b2c020bb4dee08cd
Jachacon12 <jorgech.webdeveloper@gmail.com>
2025-04-10 16:51:00 +0200

    feat: enhance staticAnalysis to evaluate branch name and abort conditionally
    Implements branching logic to determine whether to abort the pipeline based on:
    - Explicit `abortAlways` parameter
    - Current branch name (abort on `master` or branches starting with `hotfix`)
    - Default behavior for other branches

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,12 +1,31 @@
-def call() {
-    withSonarQubeEnv('Sonar local') {
-        withCredentials([string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')]) {
-            sh '''
-                sonar-scanner \
-                  -Dsonar.projectKey=devops-sonar \
-                  -Dsonar.sources=. \
-                  -Dsonar.token=$SONAR_TOKEN
-            '''
-        }
+def call(Boolean abort = false) {
+  withSonarQubeEnv('Sonar local') {
+    withCredentials([string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')]) {
+      sh '''
+        sonar-scanner \
+          -Dsonar.projectKey=devops-sonar \
+          -Dsonar.sources=. \
+          -Dsonar.token=$SONAR_TOKEN
+      '''
     }
+  }
+
+  timeout(time: 5, unit: 'MINUTES') {
+    def qg = waitForQualityGate()
+    def branch = env.BRANCH_NAME?.toLowerCase()
+
+    if (abort) {
+      if (qg.status != 'OK') {
+        error "Quality Gate failed. Pipeline aborted due to abort=true."
+      }
+    } else {
+      if (branch == 'master' || branch?.startsWith('hotfix')) {
+        if (qg.status != 'OK') {
+          error "Quality Gate failed on critical branch '${branch}'. Pipeline aborted."
+        }
+      } else {
+        echo "Quality Gate failed but branch '${branch}' is non-critical. Continuing."
+      }
+    }
+  }
 }
